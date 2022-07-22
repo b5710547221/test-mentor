f539eb851ecd9cadad8f9bdbb078d538aa34eb82
b5710547221 <warat.narat@gmail.com>
2022-07-22 23:39:19 +0700

    6th

--- pages/components/dashboard.tsx
+++ pages/components/dashboard.tsx
@@ -18,7 +18,7 @@ const data =
 const defaultProps:IObj1[] = data;
 
 export const Dashboard: React.FC  = () => {
-    const [obj1, setObj1]  = React.useState(defaultProps);
+    const [obj1, setObj1]: [IObj1[], (posts: IObj1[]) => void] = React.useState(defaultProps);
     const [search, setSearch]: [string, (search: string) => void] = React.useState("");
   
     const handleChange = (e: { target: { value: string; }; }) => {
@@ -32,12 +32,9 @@ export const Dashboard: React.FC  = () => {
            {obj1.map((obj1) => {
                if (search == "" || obj1.name.toLowerCase().includes(search.toLowerCase())) {
                    return (
-                       <li key={obj1.id}>
-                           <h3>{obj1.id}</h3>
-                           <p>{obj1.current_price}</p>
-                           <p>{obj1.symbol}</p>
-                           <img src={obj1.image} alt="image" />
-                       </li>
+                       <option key={obj1.name}>
+                           <h3>{obj1.name}</h3>
+                       </option>
                    );
                }
                return null;
